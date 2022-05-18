652e1254339250db212b4feba2ea0da0d6059ff3
PranayPandey4 <pandey.pranay1908@gmail.com>
2022-05-18 12:21:56 +0530

    push

---
+++ DevOpsServiceAPI/obj/Release/net6.0/PubTmp/EFSQLScripts/DevOpsServiceAPI.Data.DevOpsServiceAPIContext.sql
@@ -0,0 +1,35 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220513064848_InitializeData')
+BEGIN
+    CREATE TABLE [Pension] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Amount] int NOT NULL,
+        [Duration] int NOT NULL,
+        CONSTRAINT [PK_Pension] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20220513064848_InitializeData')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20220513064848_InitializeData', N'6.0.5');
+END;
+GO
+
+COMMIT;
+GO
+
